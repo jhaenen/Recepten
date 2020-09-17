483589ec0ab1030d241595a56692f05c3e09cb2d
jhaenen <jasper@haenenweb.nl>
2020-09-18 00:23:44 +0200

    Configured site with webpack and vue

---
+++ site/src/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="slxurp">Hello Vue!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+};
+</script>
+
+<style scoped>
+    body {
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    .slxurp {
+        text-align: center;
+        color: crimson;
+    }
+</style>
