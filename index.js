446be5ac595ccd3407208bb72a2ea622097e8cf1
jhaenen <jasper@haenenweb.nl>
2020-09-17 13:21:00 +0200

    Added mongodb

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
-var express = require("express");
-var app = express();
-var fs = require("fs");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const util = require("util");
+const mc = require("mongodb").MongoClient;
 
-app.get("/", function (req, res) {
-    fs.readFile("index.html", function(err, data) {
-        res.writeHead(200, {"Content-Type": "text/html"});
-        res.write(data);
-        res.end();
-    });
+var dbURL = "mongodb://localhost:27017";
+
+const readfile = util.promisify(fs.readFile);
+
+app.get("/", async function (getReq, getRes) {
+    let data;
+    try {
+        data = await readfile("index.html");
+    } catch (err) {
+        getRes.writeHead(500, "HTTP error" + err,  {"Content-Type": "text/html"});
+        return;
+    }
+
+    getRes.writeHead(200, {"Content-Type": "text/html"});
+    getRes.write(data);
+
+    const client = await mc.connect(dbURL, { useNewUrlParser: true }).catch(err => { console.log(err); });
+
+    let dbRes;
+    try {
+        const db = client.db("recepten");
+        dbRes = await db.collection("recept").find({}).toArray();
+    } catch(err) {
+        console.log(err);
+    } finally {
+        client.close();
+    }
+
+    dbRes.forEach(element => {
+        getRes.write("<div>" + element.name + "</div>");
+    });  
+    getRes.end();  
 });
 
 app.listen(2413);
